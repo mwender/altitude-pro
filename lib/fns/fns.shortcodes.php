069de05a6cb00de876225563164bdfb1f3ae5bab
Michael Wender <michael@michaelwender.com>
2015-04-15 11:34:35 -0400

    Updating shortcode to be [htmlinc]

--- lib/fns/fns.shortcodes.php
+++ lib/fns/fns.shortcodes.php
@@ -1,18 +1,21 @@
 <?php
 /**
- * Returns DJ Intelligence Availability Form HTML
+ * Returns HTML stored in lib/html/
  *
  * @since 1.0.0
  *
- * @return string Availability form HTML.
+ * @return string Specify the HTML to retrieve.
  */
-add_shortcode( 'djint', 'djint_availability' );
-function djint_availability( $atts ){
+add_shortcode( 'htmlinc', 'html_include' );
+function html_include( $atts ){
 	extract( shortcode_atts( array(
-		'form' => 'availability',
+		'html' => 'availability',
 	), $atts ) );
 
-	switch ( strtolower( $form ) ) {
+	switch ( strtolower( $html ) ) {
+		case 'awards':
+			$html = file_get_contents( dirname( __FILE__ ) . '/../html/wedding-wire-awards.html' );
+			break;
 		default:
 			$html = file_get_contents( dirname( __FILE__ ) . '/../html/djint-availability.html' );
 			break;
