e4ddd9e63db345383f094fc59f68af00f38b8b13
Michael Wender <michael@michaelwender.com>
2015-04-20 13:43:48 -0400

    Adjusting Package column heights on resize

--- js/home.js
+++ js/home.js
@@ -10,14 +10,6 @@ jQuery(function( $ ){
 
 	$( '.image-section' ) .css({'height': windowHeight +'px'});
 
-	$( window ).resize(function(){
-
-		var windowHeight = $( window ).height();
-
-		$( '.image-section' ) .css({'height': windowHeight +'px'});
-
-	});
-
 	/* Resize package columns to match 3rd column */
 	var platinum_widget_h = $( '#front-page-4 section.widget_text:nth-of-type(4)' ).height();
 	var platinum_ul_h = $( '#front-page-4 section.widget_text:nth-of-type(4) ul' ).height();
@@ -25,5 +17,19 @@ jQuery(function( $ ){
 		$( '#front-page-4 section.widget_text:nth-of-type(' + x + ')' ).height( platinum_widget_h );
 		$( '#front-page-4 section.widget_text:nth-of-type(' + x + ') ul' ).height( platinum_ul_h + 10 );
 	}
+
+	$( window ).resize(function(){
+
+		var windowHeight = $( window ).height();
+
+		$( '.image-section' ) .css({'height': windowHeight +'px'});
+
+		var platinum_widget_h = $( '#front-page-4 section.widget_text:nth-of-type(4)' ).height();
+		var platinum_ul_h = $( '#front-page-4 section.widget_text:nth-of-type(4) ul' ).height();
+		for( x = 2; x < 4; x++ ){
+			$( '#front-page-4 section.widget_text:nth-of-type(' + x + ')' ).height( platinum_widget_h );
+			$( '#front-page-4 section.widget_text:nth-of-type(' + x + ') ul' ).height( platinum_ul_h + 10 );
+		}
+	});
 
 });
