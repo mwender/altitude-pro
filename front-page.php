4fc854ce232055eb9fc04cf617294ff0ef2a2716
Michael Wender <michael@michaelwender.com>
2015-05-26 16:01:16 -0400

    Updating front-page.php ids for `friendly anchors`

--- front-page.php
+++ front-page.php
@@ -54,7 +54,7 @@ function altitude_front_page_genesis_meta() {
 			add_filter( 'body_class', 'altitude_featured_body_class' );
 			function altitude_featured_body_class( $classes ) {
 
-				$classes[] = 'featured-section';				
+				$classes[] = 'featured-section';
 				return $classes;
 
 			}
@@ -74,22 +74,22 @@ function altitude_front_page_widgets() {
 	) );
 
 	genesis_widget_area( 'front-page-2', array(
-		'before' => '<div id="front-page-2" class="front-page-2"><div class="solid-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-2' ) . '"><div class="wrap">',
+		'before' => '<div id="learn-more" class="front-page-2"><div class="solid-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-2' ) . '"><div class="wrap">',
 		'after'  => '</div></div></div></div>',
 	) );
 
 	genesis_widget_area( 'front-page-3', array(
-		'before' => '<div id="front-page-3" class="front-page-3"><div class="image-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-3' ) . '"><div class="wrap">',
+		'before' => '<div id="view-features" class="front-page-3"><div class="image-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-3' ) . '"><div class="wrap">',
 		'after'  => '</div></div></div></div>',
 	) );
 
 	genesis_widget_area( 'front-page-4', array(
-		'before' => '<div id="front-page-4" class="front-page-4"><div class="solid-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-4' ) . '"><div class="wrap">',
+		'before' => '<div id="see-pricing" class="front-page-4"><div class="solid-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-4' ) . '"><div class="wrap">',
 		'after'  => '</div></div></div></div>',
 	) );
 
 	genesis_widget_area( 'front-page-5', array(
-		'before' => '<div id="front-page-5" class="front-page-5"><div class="image-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-5' ) . '"><div class="wrap">',
+		'before' => '<div id="contact-us" class="front-page-5"><div class="image-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-5' ) . '"><div class="wrap">',
 		'after'  => '</div></div></div></div>',
 	) );
 
