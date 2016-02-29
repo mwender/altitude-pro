3adcd85c4f5498e4adabefa49a16c48a717b84d0
Michael Wender <michael@michaelwender.com>
2016-02-29 12:06:57 -0500

    Front page plans revision

--- js/home.js
+++ js/home.js
@@ -18,17 +18,17 @@ jQuery(function( $ ){
 
 	});
 
-	// Resize package columns to match 3rd column, above 1023px media query breakpoint
+	// Resize package columns to match 1st column, above 1023px media query breakpoint
 	function resizePlans(){
 		var windowWidth = $( window ).width();
 
 		if( 1023 < windowWidth ){
-			var last_widget_h = $( '#plans .widget:last-child' ).height();
-			var last_ul_h = $( '#plans .widget:last-child ul' ).height();
-			console.log('last_widget_h = ' + last_widget_h + "\nlast_ul_h = " + last_ul_h );
+			var widget_h = $( '#plans .widget:nth-child(2)' ).height();
+			var ul_h = $( '#plans .widget:nth-child(2) ul' ).height();
+			console.log('widget_h = ' + widget_h + "\nul_h = " + ul_h );
 			for( x = 2; x < 5; x++ ){
-				$( '#plans .widget:nth-of-type(' + x + ')' ).height( last_widget_h );
-				$( '#plans .widget:nth-of-type(' + x + ') ul' ).height( last_ul_h + 10 );
+				$( '#plans .widget:nth-of-type(' + x + ')' ).height( widget_h );
+				$( '#plans .widget:nth-of-type(' + x + ') ul' ).height( ul_h + 10 );
 			}
 		}
 	}
