22abc51f91c66db127466339d63c201c98c98d85
Michael Wender <michael@michaelwender.com>
2016-06-06 14:05:34 -0400

    Setting `Arvo` as font

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ function altitude_enqueue_scripts_styles() {
 	wp_enqueue_script( 'altitude-global', get_bloginfo( 'stylesheet_directory' ) . '/js/global.js', array( 'jquery' ), '1.0.0' );
 
 	wp_enqueue_style( 'dashicons' );
-	wp_enqueue_style( 'altitude-google-fonts', '//fonts.googleapis.com/css?family=Ek+Mukta:200,800', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'altitude-google-fonts', '//fonts.googleapis.com/css?family=Arvo:400,400italic,700,700italic', array(), CHILD_THEME_VERSION );
 
 }
 
