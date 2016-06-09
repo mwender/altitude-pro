dbb52dde1c2044830df6ad13b3026e9b25122f53
Michael Wender <michael@michaelwender.com>
2016-06-09 11:09:13 -0400

    Setting venue section height, and scrollTo offset

--- js/home.js
+++ js/home.js
@@ -15,13 +15,27 @@ jQuery(function( $ ){
 	var windowHeight = $( window ).height();
 
 	$( '.image-section' ) .css({'height': windowHeight +'px'});
-		
+
+	// Venue Section Height
+	var venue_h = windowHeight - 130;
+	$( '#front-page-6 .solid-section' ) .css({'height': venue_h +'px'});
+	$( '#front-page-6 iframe' ) . css({'height': venue_h +'px'});
+
+	// Scroll to Venue section
+	var venuesection = $('#front-page-6');
+	$('li.venue a').click(function(e){
+		e.preventDefault();
+		$.scrollTo(venuesection,750,{
+			offset: -80
+		});
+	});
+
 	$( window ).resize(function(){
-	
+
 		var windowHeight = $( window ).height();
-	
+
 		$( '.image-section' ) .css({'height': windowHeight +'px'});
-	
+
 	});
 
 });
