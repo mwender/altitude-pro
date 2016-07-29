4e4a608725465c216d307d51df68ea27bd19f21e
Michael Wender <michael@michaelwender.com>
2016-07-29 14:36:47 -0400

    Updating #front-page-5 to #registration

--- front-page.php
+++ front-page.php
@@ -90,7 +90,7 @@ function altitude_front_page_widgets() {
 	) );
 
 	genesis_widget_area( 'front-page-5', array(
-		'before' => '<div id="front-page-5" class="front-page-5"><div class="image-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-5' ) . '"><div class="wrap">',
+		'before' => '<div id="registration" class="front-page-5"><div class="image-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-5' ) . '"><div class="wrap">',
 		'after'  => '</div></div></div></div>',
 	) );
 
