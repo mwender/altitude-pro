b1386f7fda56a74443165e7c233925b8ccbf70dd
Michael Wender <michael@michaelwender.com>
2016-08-22 11:15:36 -0400

    Loading script via http | https

--- functions.php
+++ functions.php
@@ -47,7 +47,7 @@ function altitude_enqueue_scripts_styles() {
 
 	if( is_front_page() ){
 		wp_enqueue_script( 'jquery-ui-datepicker' );
-		wp_enqueue_style('jquery-ui-datepicker-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css');
+		wp_enqueue_style('jquery-ui-datepicker-style', '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css');
 	}
 
 	// Register Availability Tracker Scripts
