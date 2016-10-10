b51ad43cbae8a18437e21623f7ea70e8814f9479
Michael Wender <michael@michaelwender.com>
2016-10-10 13:33:42 -0400

    Changing front-page-3 to auto height

--- front-page.php
+++ front-page.php
@@ -71,7 +71,7 @@ function altitude_front_page_widgets() {
 	) );
 
 	genesis_widget_area( 'front-page-3', array(
-		'before' => '<div id="front-page-3" class="front-page-3"><div class="image-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-3' ) . '"><div class="wrap">',
+		'before' => '<div id="front-page-3" class="front-page-3"><div class="solid-section"><div class="flexible-widgets widget-area' . altitude_widget_area_class( 'front-page-3' ) . '"><div class="wrap">',
 		'after'  => '</div></div></div></div>',
 	) );
 
