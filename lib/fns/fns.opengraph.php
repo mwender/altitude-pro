637d6745f5435f1988ba4ee9b1c3e26efef3e823
Michael Wender <michael@michaelwender.com>
2017-03-08 11:24:18 -0500

    Updating social sharing image

--- lib/fns/fns.opengraph.php
+++ lib/fns/fns.opengraph.php
@@ -8,7 +8,7 @@ namespace Genesis\AltitudePro\fns\opengraph;
  */
 function open_graph_image(){
 ?>
-<meta property="og:image" content="<?php  echo get_stylesheet_directory_uri() ?>/images/edge2016-social-thumbnail.1400x655.jpg">
+<meta property="og:image" content="<?php  echo get_stylesheet_directory_uri() ?>/images/edge2017-social-thumbnail.1400x655.jpg">
 <?php
 }
 add_action( 'wp_head', __NAMESPACE__ . '\\open_graph_image', 5 );
