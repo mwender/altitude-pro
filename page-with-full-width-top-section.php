6086e132463b1f9544a247fc56b3a91ced6f1a7e
Michael Wender <michael@michaelwender.com>
2018-08-03 10:01:02 -0400

    Adding Nashville registration page

--- page-with-full-width-top-section.php
+++ page-with-full-width-top-section.php
@@ -3,9 +3,11 @@
  Template Name: Full width top section
  */
 
-add_action( 'genesis_header', 'full_width_top_section',99 );
 function full_width_top_section(){
-	echo '<div class="top-section"><div class="gradient">' . do_shortcode( '[htmlinc html="top-section" /]' ) . '</div></div>';
+  global $post;
+  $html = ( $top_section = get_post_meta( $post->ID, 'top_section', true ) )? $top_section : 'top-section';
+	echo '<div class="top-section"><div class="gradient">' . do_shortcode( '[htmlinc html="' . $html . '" /]' ) . '</div></div>';
 }
+add_action( 'genesis_header', 'full_width_top_section', 99 );
 
 genesis();
